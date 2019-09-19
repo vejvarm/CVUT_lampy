8dc4335d2c19b6e6c8951c84680973b1646e6add
Martin Vejvar <vejvarm@gmail.com>
2019-09-19 11:35:06 +0200

    feat: data comparison and logarithmic plots

--- main.py
+++ main.py
@@ -9,39 +9,52 @@ from Aggregators import Average, complete_average
 
 
 if __name__ == '__main__':
-    path_list = ['./data/neporuseno/week']
-    # path_list = ['./data/ruzne/PovlakPrycSloup1OdleptaniSloup2Sloup3__2019_03_28___2019_07_01__vz/week']
-    averages_dict = dict()
-    # Preprocess files referenced by path_list
-    preproc = Preprocessor(noise_f_rem=(2, 50, 100, 150, 200),
-                           noise_df_rem=(2, 5, 1, 5, 1),
-                           mov_filt_size=5)  # refer to __init__ for possible preprocessing settings
-    preprocessed = preproc.run(path_list)
-#    print(preprocessed.keys())
-#    print(preprocessed['12102018_AccM'])
+    path_list = [['./data/neporuseno/week'],
+                 ['./data/PovlakPrycSloup1OdleptaniSloup2Sloup3__2019_03_28___2019_07_01__vz/week']]
+    psd_day = 0
+    for path in path_list:
+        averages_dict = dict()
+        # Preprocess files referenced by path_list
+        preproc = Preprocessor(noise_f_rem=(2, 50, 100, 150, 200),
+                               noise_df_rem=(2, 5, 1, 5, 1),
+                               mov_filt_size=5)  # refer to __init__ for possible preprocessing settings
+        preprocessed = preproc.run(path)
+    #    print(preprocessed.keys())
+    #    print(preprocessed['12102018_AccM'])
 
-    num_accs = len(preprocessed[list(preprocessed.keys())[0]][1])
-    long_term_aggs = [Average() for _ in range(num_accs)]
-    for key, value in preprocessed.items():
-        freq_vals, psd_list, _, _ = value
-        daily_avg = [[]] * len(psd_list)
-        for i, psd in enumerate(psd_list):
-            daily_avg[i] = complete_average(psd)    # in production use Aggregators.Average class for online averaging
-            long_term_aggs[i].update(daily_avg[i])  # aggregate the long term average (PSD) of the psd of accelerations
+        num_accs = len(preprocessed[list(preprocessed.keys())[0]][1])
+        long_term_aggs = [Average() for _ in range(num_accs)]
+        for key, value in preprocessed.items():
+            freq_vals, psd_list, _, _ = value
+            daily_avg = [[]] * len(psd_list)
+            for i, psd in enumerate(psd_list):
+                daily_avg[i] = complete_average(psd)    # in production use Aggregators.Average class for online averaging
+                long_term_aggs[i].update(daily_avg[i])  # aggregate the long term average (PSD) of the psd of accelerations
 
-        averages_dict[key] = daily_avg  # save calculated daily psd averages of acc[0:6]
+            averages_dict[key] = daily_avg  # save calculated daily psd averages of acc[0:6]
 
-#    plt.plot(freq_vals, acc1_psd_denoised[:, 0])
-    _, ax = plt.subplots(3, 2)
-    ax = ax.flatten()
-    for i, agg in enumerate(long_term_aggs):
-        ax[i].plot(freq_vals, agg.PSD)
-        ax[i].set_xlabel('frequency [Hz]')
-        ax[i].set_ylabel('PSD')
-        ax[i].set_title(f'sloup {i//2 + 1} acc {i%2 + 1}')
+        ## PLOTTING:
+        # LONG TERM AVERAGES (PSD)
+        fig, ax = plt.subplots(3, 2)
+        ax = ax.flatten()
+        for i, agg in enumerate(long_term_aggs):
+            fig.suptitle('LONG TERM AVERAGES (PSD)', fontsize=16)
+            ax[i].semilogy(freq_vals, agg.PSD)
+            ax[i].set_xlabel('frequency [Hz]')
+            ax[i].set_ylabel('PSD')
+            ax[i].set_title(f'sloup {i//2 + 1} acc {i%2 + 1}')
 
-#    for value in averages_dict.values():
-#        plt.plot(freq_vals, value[0])
+        # SHORT TERM AVERAGES (psd)
+        for i, value in enumerate(averages_dict.values()):
+            if i == psd_day:
+                fig, ax = plt.subplots(3, 2)
+                ax = ax.flatten()
+                fig.suptitle('SHORT TERM AVERAGES (psd)', fontsize=16)
+                for i, psd in enumerate(value):
+                    ax[i].semilogy(freq_vals, psd)
+                    ax[i].set_xlabel('frequency [Hz]')
+                    ax[i].set_ylabel('psd')
+                    ax[i].set_title(f'sloup {i // 2 + 1} acc {i % 2 + 1}')
 
     plt.show()
 
--- scratch/wind_spd_animation.py
+++ scratch/wind_spd_animation.py
@@ -1,3 +1,5 @@
+# TODO: graf větru inicializovat již s hodnotami a poté jen měnit barvu v okolí aktuálního 'j'
+
 import numpy as np
 
 from matplotlib import pyplot as plt
@@ -6,7 +8,7 @@ from matplotlib import animation as animation
 from preprocessing import Preprocessor
 
 # Loading data
-path_list = ['D:/!private/Lord/Git/CVUT_lampy/data/neporuseno/week']
+path_list = ['../data/neporuseno/week']
 preproc = Preprocessor(noise_f_rem=(2, 50, 100, 150, 200),
                            noise_df_rem=(2, 5, 1, 5, 1),
                            mov_filt_size=5)  # refer to __init__ for possible preprocessing settings
@@ -28,28 +30,43 @@ line = list()
 
 # acc plots (first 3 rows of figure)
 for i in range(n_accs):
-    ax.append(fig.add_subplot(grid[i//2, i%2]))
-    line.append(ax[i].plot(freqs, np.zeros(freqs.shape))[0])
+    ax.append(fig.add_subplot(grid[i//2 + 1, i % 2]))
+    line.append(ax[i].semilogy(freqs, np.ones(freqs.shape))[0])
+    for j in range(n_msrmnts):
+        acc = accs[i][:, j]
+        acc_min, acc_max = (np.min(acc), np.max(acc))
+#        mean = np.mean(accs[i][:, j])
+#        std = np.std(accs[i][:, j])
+        acc = (acc - acc_min)/(acc_max - acc_min)
 
 # wind speed plot (4th row of figure)
-ax.append(fig.add_subplot(grid[3, :]))
-line.append(ax[-1].plot(np.arange(n_msrmnts), np.zeros(n_msrmnts))[0])
+idx = np.arange(n_msrmnts)
+ax.append(fig.add_subplot(grid[0, :]))
+line.append(ax[-1].plot(idx, np.zeros(n_msrmnts))[0])  # 1 for consistent line
+line.append(ax[-1].plot(idx, np.zeros(n_msrmnts))[0])  # 1 for current position
 
 def animate_init():
     for i in range(n_accs):
         line[i].set_ydata([np.nan]*len(freqs))
-        ax[i].set_ylim(0, np.max(accs[i]))
+        line[i].set_color("red")
+        ax[i].set_ylim(np.min(accs[i]), np.max(accs[i]))
     line[-1].set_ydata([np.nan]*n_msrmnts)
-    ax[-1].set_ylim(0, np.max(wind_spd))
+    line[-1].set_color("red")
+    line[-1].set_linewidth(2)
+    line[-2].set_ydata(wind_spd)
+    ax[-1].set_ylim(-0.2, np.max(wind_spd))
     return line
 
 def animate(j):
     for i in range(n_accs):
         line[i].set_ydata(accs[i][:, j])
-    line[-1].set_ydata(np.hstack((wind_spd[:j], [np.nan]*(n_msrmnts - j))))
+#    line[-1].set_ydata(np.hstack((wind_spd[:j], [np.nan]*(n_msrmnts - j))))
+    line[-1].set_data(idx[j-5:j], wind_spd[j-5:j])
+#    ax[-1].axvspan(idx[j], idx[j+3], color="red")
     return line
 
 if __name__ == '__main__':
-    ani = animation.FuncAnimation(fig, animate, init_func=animate_init, frames=n_msrmnts, interval=100, blit=True, save_count=50, repeat=True)
+    ani = animation.FuncAnimation(fig, animate, init_func=animate_init, frames=n_msrmnts, interval=200,
+                                  blit=True, save_count=50, repeat=True)
 
     plt.show()
