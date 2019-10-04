3ae74270b68f08fa646222ecb379d769d6fca62f
Martin Vejvar <vejvarm@gmail.com>
2019-10-04 15:31:08 +0200

    feat: add function to binarize psd arrays

--- scratch/multiscale.py
+++ scratch/multiscale.py
@@ -4,6 +4,23 @@ from matplotlib import pyplot as plt
 from preprocessing import Preprocessor
 
 
+def binarize(psd_array, threshhold):
+    """ make binarized psd array from input psd_array
+
+    :param psd_array: (ndarray)
+    :param threshhold: (float)
+    :return: psd_binarized
+    """
+
+    bins = np.array([0.0, threshhold])
+
+    psd_binarized = np.array(psd_array)
+    psd_binarized[psd_binarized < threshhold] = 0.0
+    psd_binarized[psd_binarized >= threshhold] = 1.0
+
+    return psd_binarized
+
+
 def mean_to_bins(psd_array, bin_size):
     """Vezme pole psd_array a rozdělí ho na košíky (podle frekvence).
     hodnoty košíku se zprůměrují do jedné hodnoty
@@ -46,7 +63,7 @@ if __name__ == "__main__":
 #    filename = "08072018_AccM"
 #    path = ['../data/neporuseno/2months/' + filename + ".mat"]
 
-    path = ["../data/neporuseno2/2months/"]
+    path = ["../data/neporuseno/week/"]
 
     preprocessed = preproc.run(path)
 
@@ -64,6 +81,9 @@ if __name__ == "__main__":
     bin_size = 26
 
     psd_bins = mean_to_bins(psd_average, bin_size)
+    psd_binarized = binarize(psd_bins, psd_bins.mean())
+
+    psd_binarized_aggregated = psd_binarized.sum(axis=2)
 
     print(psd_average.shape)
     print(psd_bins.shape)
@@ -85,7 +105,18 @@ if __name__ == "__main__":
         im = a.pcolormesh(fbins, msrmnts, psd_bins[i, :, :].T)
     fig2.colorbar(im, ax=ax2)
 
-    fig.savefig('../results/fig.png', dpi=300)
-    fig2.savefig('../results/fig2.png', dpi=300)
+    fig3, ax3 = plt.subplots(3, 2)
+    for i, a in enumerate(ax3.flatten()):
+        im = a.pcolormesh(fbins, msrmnts, psd_binarized[i, :, :].T)
+    fig3.colorbar(im, ax=ax3)
+
+    fig4, ax4 = plt.subplots(3, 2)
+    for i, a in enumerate(ax4.flatten()):
+        plot = a.stem(fbins, psd_binarized_aggregated[i, :], use_line_collection=True, markerfmt=" ")
+
+    fig.savefig('../results/original.png', dpi=300)
+    fig2.savefig('../results/binned.png', dpi=300)
+    fig3.savefig('../results/binarized.png', dpi=300)
+    fig4.savefig('../results/binarized_aggregated.png', dpi=300)
 
     plt.show()
